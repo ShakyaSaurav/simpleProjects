39aaea08cd22b70411580a5a7c64a756603687fc
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-07-29 21:10:00 +0545

    adding the js

---
+++ Counter/app.js
@@ -0,0 +1,8 @@
+//setting initial value yesko
+let count = 0;
+const value = document.querySelectorAll('#value');
+const buttons= document.querySelectorAll('.btn');
+//console.log(buttons);
+buttons.forEach(function(item){
+    console.log('item');
+});
