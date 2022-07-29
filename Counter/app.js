8147bb6a9edcacde94dfe2fc0b878c22ce7f41ce
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-07-29 21:28:47 +0545

    left at null class target

--- Counter/app.js
+++ Counter/app.js
@@ -1,14 +1,22 @@
 //setting initial value yesko
 let count = 0;
 const value = document.querySelectorAll('#value');
-const buttons= document.querySelectorAll('.btn');
+const btns= document.querySelectorAll('.btn');
 //console.log(buttons);
-buttons.forEach(function(item){
+btns.forEach(function(item){
 //console.log('item');
- item.addEventListener('click',function(e){
-    const changes=e.currentTarget.classList;
-    if(changes.contains('decrease')){
+ item.addEventListener('click',function (e) {
+    const styles = e.currentTarget.classlist;
+    //console.log(e);
+    if (styles.contains('decrease')){
         count--;
     }
- })
+    else if (styles.contains('reset')){
+        count=0;
+    }
+    else if (styles.contains('increase')){
+        count++;
+    }
+value.textContent = count;
+ });
 });
