83ae7f1db405132ec8ca3e05523082d0e73c871d
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-07-30 12:18:09 +0545

    bug found... got it working

--- Counter/app.js
+++ Counter/app.js
@@ -1,22 +1,22 @@
 //setting initial value yesko
 let count = 0;
-const value = document.querySelectorAll('#value');
+const value = document.querySelector('#value');
 const btns= document.querySelectorAll('.btn');
 //console.log(buttons);
 btns.forEach(function(item){
 //console.log('item');
  item.addEventListener('click',function (e) {
-    const styles = e.currentTarget.classlist;
-    //console.log(e);
-    if (styles.contains('decrease')){
-        count--;
-    }
-    else if (styles.contains('reset')){
-        count=0;
-    }
-    else if (styles.contains('increase')){
-        count++;
-    }
-value.textContent = count;
+   //console.log(e.currentTarget.classList);
+  const changes= e.currentTarget.classList;
+  if (changes.contains('decrease')){
+    count--;
+  }
+  else if(changes.contains('reset')){
+    count=0;
+  }
+  else if(changes.contains('increase')){
+    count++;
+  }
+  value.textContent = count;
  });
 });
