5ee3f6465dc6dd1fe9722ff0a9498a1fdff68da7
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-07-31 21:08:59 +0545

    add js

---
+++ Review display/app.js
@@ -0,0 +1,14 @@
+const review=[
+    {
+        id:1,
+        name:"Saurav Shakya",
+        job:"Web developer",
+        img:"C:\Users\HP B&O\Desktop\IMG_9224.JPG",
+        text:"asdkjadshjhjadshjhjdhjjhadsjhhjadjhjadshjdjhjhadhjjhhjadshjhjhjadsjhhjadsjh",
+    },
+];
+const img=document.getElementById("person-img");
+const author=document.getElementById("author");
+const job=document.getElementById("job");
+const info=document.getElementById("info");
+
