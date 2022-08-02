ea00a22aed7333dcf3129c4873dd073f0e0d923a
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-02 19:55:12 +0545

    add remove

--- Nav Bar/app.js
+++ Nav Bar/app.js
@@ -3,4 +3,9 @@ const links=document.querySelector('.links');
 
 navToggle.addEventListener('click',function(){
 //console.log(links.classList);
+if(links.classList.contains('show-links')){
+    links.classList.remove('show-links');
+}else{
+    links.classList.add("showlinks");
+}
 });
