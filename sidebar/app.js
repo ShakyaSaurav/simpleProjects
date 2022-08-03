0f2738ccc0e8f45d34223a54346d2bfe12944587
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-03 17:27:22 +0545

    logo arranged

--- sidebar/app.js
+++ sidebar/app.js
@@ -6,3 +6,7 @@ toggleBtn.addEventListener("click",function(){
 //console.log(sidebar.classList);
 sidebar.classList.toggle('show-sidebar');
 });
+
+closeBtn.addEventListener("click",function(){
+    sidebar.classList.remove('show-sidebar');
+});
