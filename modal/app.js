3227bb298a3ee2724b60c94eae4a22b160283207
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-05 20:54:15 +0545

    got it working

--- modal/app.js
+++ modal/app.js
@@ -2,7 +2,7 @@ const modalBtn= document.querySelector('.modal-btn');
 const modal= document.querySelector('.modal-overlay');
 const closeBtn= document.querySelector('.close-btn');
 modalBtn.addEventListener("click",function(){
-    modal.classlist.add('open-modal');
+    modal.classList.add('open-modal');
 });
 closeBtn.addEventListener("click",function(){
     modal.classList.remove('open-modal');
