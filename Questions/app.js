48e2911cac5584f3da39cf782f1634b86a3aeee3
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-07 20:42:38 +0545

    initial app.js

---
+++ Questions/app.js
@@ -0,0 +1,6 @@
+const question = document.querySelectorAll('.question');
+question.forEach(function(orange){
+    //console.log('orange');
+    const btn= question.querySelectorAll('.question-btn');
+    console.log('btn');
+});
