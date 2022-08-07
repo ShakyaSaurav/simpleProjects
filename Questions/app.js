a669ac628d2d1b7251c3303a3d9a357b4679ec54
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-07 20:57:27 +0545

    questions completed

--- Questions/app.js
+++ Questions/app.js
@@ -1,9 +1,14 @@
-const question = document.querySelectorAll('.question');
-question.forEach(function(question){
+const questions = document.querySelectorAll('.question');
+questions.forEach(function(question){
     //console.log('question');
     const btn = question.querySelector('.question-btn');
     //console.log(btn);
    btn.addEventListener('click',function(){
+    questions.forEach(function(item){
+        if (item!==question){
+            item.classList.remove('show-text');
+        }
+    });
     question.classList.toggle('show-text');
    });
     
