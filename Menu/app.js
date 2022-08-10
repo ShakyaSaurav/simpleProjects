48735d815b4a714db0a28a2fdeb56d1f9eb993bc
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-10 20:37:59 +0545

    adding menu items

---
+++ Menu/app.js
@@ -0,0 +1,34 @@
+const menu=[
+{
+    id:1,
+    title:'Cheese Pizza',
+    category:'lunch',
+    price:450,
+    img:'./images/item1.jpg',
+    desc:'Yummy plain pizza with tomato sauce and mozarella cheese on top. Baked on a wood-fire oven.',
+}, 
+{
+    id:2
+    title:'Banana toast',
+    category:'breakfast',
+    price:150,
+    img:'./images/item2.jpg',
+    desc:'bread toasted on ghee and topped with fresh banana and chocolate syrup',
+}, 
+{
+    id:3
+    title:'',
+    category:'',
+    price:,
+    img:'',
+    desc:'',
+}, 
+{
+    id:4
+    title:'',
+    category:'',
+    price:
+    img:'',
+    desc:'',
+},    
+]
