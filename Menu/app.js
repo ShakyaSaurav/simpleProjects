3f3c4a246cd1251cfcb407f0f5181d81f60f83c0
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-11 19:43:36 +0545

    got it working

--- Menu/app.js
+++ Menu/app.js
@@ -34,23 +34,25 @@ const menu=[
 ];
 const sectionCenter=document.querySelector('.section-center');
 window.addEventListener('DOMContentLoaded',function(){
+    displayMenuItems(menu);
  //console.log('cooking');
- 
- displayMenuItems(menu);
 });
 
 function displayMenuItems(menuItems){
-    let displayMenu= menuItems.map(function(item){
-        return `<article class="menu-item">
-        <img src="${item.img}" class="photo" alt="${item.title}">
-        <div class="item-info">
-            <header>
-            <h4>${item.title}</h4>
-            <h4 class="price">NRS ${item.price}</h4>
-        </header>
-        <p class="item-text">${item.desc}
-        </p>
-        </div>
-    </article>`;
-     });
-}
+    
+let displayMenu= menuItems.map(function(item){
+    return `<article class="menu-item">
+    <img src="${item.img}" class="photo" alt="${item.title}">
+    <div class="item-info">
+        <header>
+        <h4>${item.title}</h4>
+        <h4 class="price">NRS ${item.price}</h4>
+    </header>
+    <p class="item-text">${item.desc}
+    </p>
+    </div>
+</article>`;
+ });
+ displayMenu =displayMenu.join("");
+ sectionCenter.innerHTML =displayMenu;
+};
