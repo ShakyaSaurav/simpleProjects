ffc22b9ccb1fd5fa441196d33bdd6b4ef717ec5f
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-16 20:36:09 +0545

    adding js

---
+++ scrollProject/app.js
@@ -0,0 +1,8 @@
+//setting date
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+//closelinks
+const navToggle=document.querySelector('.nav-toggle');
+const linksContainer=document.querySelector('.links-container');
+const links=document.querySelector('.links');
