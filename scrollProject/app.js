85af2d6b418987c1d5f520aa6d58afd2315b9f7f
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-18 18:10:07 +0545

    added images

--- scrollProject/app.js
+++ scrollProject/app.js
@@ -12,5 +12,10 @@ navToggle.addEventListener('click',function(){
     const containerHeight =linksContainer.getBoundingClientRect();
     //console.log(containerHeight);
     const linksHeight = links.getBoundingClientRect().height;
-    console.log(linksHeight);
+    //console.log(linksHeight);
+    if(containerHeight===0){
+        linksContainer.style.height= `${linksHeight}px`;
+    } else{
+        linksContainer.style.height= 0 ;
+    }
 });
