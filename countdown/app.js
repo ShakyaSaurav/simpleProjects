80c515df31b77255a003131511be9280521a68cd
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-08-31 20:11:26 +0545

    dates dynamic phase 1asd

--- countdown/app.js
+++ countdown/app.js
@@ -21,3 +21,16 @@ const weekdays=[
     "Friday",
     "Saturday",
 ];
+const giveaway = document.querySelector('.giveaway');
+const deadline= document.querySelector('.deadline');
+const items =document.querySelectorAll('.deadline-format h4');
+//console.log(items);
+let futureDate= new Date(2023,4,24,11,30,0);
+//console.log(futureDate);
+const year= futureDate.getFullYear();
+const hours=futureDate.getHours();
+const minutes=futureDate.getMinutes();
+
+let month= futureDate.getMonth();
+console.log(months[month]);
+giveaway.textContent=`Giveaway ends on ${year} ${hours}:${minutes}am`;
