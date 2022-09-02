21888aec3548cb25faba478bcfdb25fa7186679c
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-09-02 20:55:23 +0545

    add 10 day but ez

--- countdown/app.js
+++ countdown/app.js
@@ -24,8 +24,15 @@ const weekdays=[
 const giveaway = document.querySelector('.giveaway');
 const deadline= document.querySelector('.deadline');
 const items =document.querySelectorAll('.deadline-format h4');
+
+let tempDate= new Date();
+let tempYear = tempDate.getFullYear();
+let tempMonth = tempDate.getMonth();
+let tempDay =tempDate.getDate();
 //console.log(items);
-let futureDate= new Date(2023,4,24,11,30,0);
+
+//let futureDate= new Date(2023,4,24,11,30,0);
+const futureDate=new Date(tempYear,tempMonth,tempDay+10,11,30,0);
 //console.log(futureDate);
 const year= futureDate.getFullYear();
 const hours= futureDate.getHours();
