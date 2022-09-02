e8711c8ad834ab26e34571d6079e4e2f7d336af4
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-09-02 20:51:29 +0545

    project complete

--- countdown/app.js
+++ countdown/app.js
@@ -57,7 +57,27 @@ const oneMinute=60*1000;
 //calculate all values;
 let days= t/oneDay;
 days= Math.floor(days);
-let hours = t/oneHour;
+let hours = (t%oneDay)/oneHour;
+hours= Math.floor(hours);
+let minutes= Math.floor((t%oneHour)/ oneMinute);
+let seconds = Math.floor((t%oneMinute/1000));
+const values= [days,hours,minutes,seconds];
 
+function format(item){
+    if(item<10){
+        return (item = `0${item}`);
+    }
+    return item;
 }
+
+items.forEach(function(item,index){
+ item.innerHTML=values[index];
+});
+if(t<0){
+    clearInterval(countdown);
+    deadline.innerHTML=`<h4 class="expired"> sorry tis giveaway has expired <h4>`
+}
+};
+let countdown = setInterval(getRemainingTime,1000);
+
 getRemainingTime();
