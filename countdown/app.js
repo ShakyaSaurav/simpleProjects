a22bd6935fa88aac059ffcc6b3fe0c71b0e4ac26
ShakyaSaurav <sauravshakya2015@gmail.com>
2022-09-02 20:25:50 +0545

    timing

--- countdown/app.js
+++ countdown/app.js
@@ -28,10 +28,13 @@ const items =document.querySelectorAll('.deadline-format h4');
 let futureDate= new Date(2023,4,24,11,30,0);
 //console.log(futureDate);
 const year= futureDate.getFullYear();
-const hours=futureDate.getHours();
-const minutes=futureDate.getMinutes();
+const hours= futureDate.getHours();
+const minutes = futureDate.getMinutes();
 
 let month= futureDate.getMonth();
-console.log(months[month]);
+//console.log(months[month]);
+month = months[month];
 const date= futureDate.getDate();
-giveaway.textContent=`Giveaway ends on ${month} ${year} ${hours}:${minutes}am`;
+
+const weekday= weekdays[futureDate.getDay()];
+giveaway.textContent=`Giveaway ends on ${weekday},${date} ${month} ${year} ${hours}:${minutes}am`;
